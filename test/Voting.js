9d28a04bc422cadcc7f24c969eccc3b48e0e4383
Alexandre EL-KHOURY <alexkhoury@hotmail.com>
2022-04-05 15:58:41 +0200

    updated

--- test/Voting.js
+++ test/Voting.js
@@ -16,13 +16,17 @@ contract("Voting", function (accounts) {
      //Step 1: Registering Voters.
     
     describe("1 --- Registering Voters ---", function () {
-        beforeEach(async function () {
+        before(async function () {
             VotingInstance = await Voting.new({ from: owner });
         });
 
         it("should make sure status is at registering voters", async function () {
             currentState = await VotingInstance.workflowStatus();
             expect(currentState).to.be.bignumber.equal(new BN(0));
+        });
+
+        it("shouldn't add a voter if it is not the owner", async function() {
+            await expectRevert(VotingInstance.addVoter(voter2, { from: voter1 }), "Ownable: caller is not the owner");
         });
 
         it("should add a voter and emit event", async function() {
@@ -33,27 +37,30 @@ contract("Voting", function (accounts) {
         });
 
         it("shouldn't add a voter if the voter is already registered", async function() {
-            await VotingInstance.addVoter(voter1, { from: owner });
             await expectRevert(VotingInstance.addVoter(voter1, { from: owner }), "Already registered");
-        });
-
-        it("shouldn't add a voter if it is not the owner", async function() {
-            await expectRevert(VotingInstance.addVoter(voter2, { from: voter1 }), "Ownable: caller is not the owner");
         });
 
         it("shouldn't add a voter if the workflow status is not set at RegisteringVoters", async function() {
             VotingInstance.startProposalsRegistering({ from: owner });
-            await expectRevert(VotingInstance.addVoter(voter1, { from: owner }), "Voters registration is not open yet");
+            await expectRevert(VotingInstance.addVoter(voter2, { from: owner }), "Voters registration is not open yet");
         });
     });
 
      //Step 2: Proposals Registration Started.
      
     describe("2 --- Proposals Registration Started ---", function () {
-        beforeEach(async function () {
+        before(async function () {
             VotingInstance = await Voting.new({ from: owner });
             await VotingInstance.addVoter(voter1, { from: owner });
             await VotingInstance.addVoter(voter2, { from: owner });
+        });
+
+        it("shouldn't start the proposals registering if it is not the owner", async function() {
+            await expectRevert(VotingInstance.startProposalsRegistering({ from: voter1 }),"Ownable: caller is not the owner");
+        });
+
+        it("shouldn't add a proposal if the workflow status is not set to startProposalsRegistering", async function() {
+            await expectRevert(VotingInstance.addProposal(proposalDescription1, { from: voter1 }),"Proposals are not allowed yet");
         });
 
         it("should start proposal registration", async function () {
@@ -62,42 +69,32 @@ contract("Voting", function (accounts) {
             expect(currentState).to.be.bignumber.equal(new BN(1));
         });
 
-        it("shouldn't start the proposals registering if it is not the owner", async function() {
-            await expectRevert(VotingInstance.startProposalsRegistering({ from: voter1 }),"Ownable: caller is not the owner");
-        });
-
-        it("shouldn't start the proposals registering if the workflow status is not set at RegisteringVoters", async function() {
-            await VotingInstance.startProposalsRegistering({ from: owner });
-            await VotingInstance.endProposalsRegistering({ from: owner });
-            await expectRevert(VotingInstance.startProposalsRegistering({ from: owner }), "Registering proposals cant be started now");
+        it("shouldn't add a proposal if it is not a voter", async function() {
+            await expectRevert(VotingInstance.addProposal(proposalDescription1, { from: notVoter }), "You're not a voter");
         });
 
         it("should add a proposal and emit event", async function() {
-            await VotingInstance.startProposalsRegistering({ from: owner });
             const findEvent = await VotingInstance.addProposal(proposalDescription1, { from: voter1 });
             const proposal = await VotingInstance.getOneProposal(0, { from: voter1 });
             expect(proposal.description).to.equal(proposalDescription1);
             expectEvent(findEvent, "ProposalRegistered", { proposalId: new BN(0) });
         });
 
-        it("shouldn't add a proposal if it is not a voter", async function() {
-            await VotingInstance.startProposalsRegistering({ from: owner });
-            await expectRevert(VotingInstance.addProposal(proposalDescription1, { from: notVoter }), "You're not a voter");
-        });
-
-        it("shouldn't add a proposal if the workflow status is not set to RegisteringVoters", async function() {
-            await VotingInstance.startProposalsRegistering({ from: owner });
-            await VotingInstance.endProposalsRegistering({ from: owner });
-            await expectRevert(VotingInstance.addProposal(proposalDescription1, { from: voter1 }),"Proposals are not allowed yet");
+        it("shouldn't start the proposals registering if the workflow status is not set at RegisteringVoters", async function() {
+            await expectRevert(VotingInstance.startProposalsRegistering({ from: owner }), "Registering proposals cant be started now");
         });
     });
 
      // Step 3: Ending Proposals Registrations.
      
     describe("3 --- Ending Proposals Registrations ---", function () {
-        beforeEach(async function () {
+        before(async function () {
             VotingInstance = await Voting.new({ from: owner });
             await VotingInstance.addVoter(voter1, { from: owner });
+        });
+
+        it("shouldn't end the proposals registering if the workflow status is not set at ProposalsRegistrationStarted", async function() {
+            await expectRevert(VotingInstance.endProposalsRegistering({ from: owner}),"Registering proposals havent started yet");
         });
 
         it("shouldn't end the proposals registering if it is not the owner", async function() {
@@ -105,12 +102,7 @@ contract("Voting", function (accounts) {
             await expectRevert(VotingInstance.endProposalsRegistering({ from: voter1 }), "Ownable: caller is not the owner");
         });
 
-        it("shouldn't end the proposals registering if the workflow status is not set at ProposalsRegistrationStarted", async function() {
-            await expectRevert(VotingInstance.endProposalsRegistering({ from: owner}),"Registering proposals havent started yet");
-        });
-
         it("should end proposal registration", async function () {
-            await VotingInstance.startProposalsRegistering({ from: owner });
             await VotingInstance.endProposalsRegistering({from : owner});
             currentState = await VotingInstance.workflowStatus();
             expect(currentState).to.be.bignumber.equal(new BN(2));
@@ -120,7 +112,7 @@ contract("Voting", function (accounts) {
      //Step 4: Voting Session Started.
      
     describe("4 --- Voting Session Started ---", function () {
-        beforeEach(async function () {
+        before(async function () {
             VotingInstance = await Voting.new({ from: owner });
             await VotingInstance.addVoter(voter1, { from: owner });
             await VotingInstance.startProposalsRegistering({ from: owner });
@@ -128,14 +120,27 @@ contract("Voting", function (accounts) {
             await VotingInstance.endProposalsRegistering({ from: owner });
         });
 
-        it("should change status to start voting", async function () {
+        it("shouldn't set a vote if the workflow status is not set at VotingSessionStarted", async function() {
+            await expectRevert(VotingInstance.setVote(0, { from: voter1 }),"Voting session havent started yet");
+        });
+
+        it("shouldn't start the voting session if it is not the owner", async function() {
+            await expectRevert(VotingInstance.startVotingSession({ from: voter1 }),"Ownable: caller is not the owner");
+        });
+
+        it("should start voting the voting session", async function () {
             await VotingInstance.startVotingSession();
             currentState = await VotingInstance.workflowStatus();
             expect(currentState).to.be.bignumber.equal(new BN(3));
         });
 
+        // Je teste si on peut 'startVotingSession' si celle-ci à déjà commencer. 
+        // Le message revert de la correction n'est donc pas très adapté mais le test passe :)
+        it("shouldn't start the voting session if the workflow status is not set at ProposalsRegistrationEnded", async function() {
+            await expectRevert(VotingInstance.startVotingSession(), "Registering proposals phase is not finished");
+        });
+
         it("should set a vote and emit event", async function() {
-            await VotingInstance.startVotingSession({ from: owner });
             const findEvent = await VotingInstance.setVote(0, { from: voter1 });
             const voter = await VotingInstance.getVoter(voter1, { from: voter1 });
             const proposal = await VotingInstance.getOneProposal(0, { from: voter1 });
@@ -146,30 +151,12 @@ contract("Voting", function (accounts) {
             expectEvent(findEvent, "Voted", { voter: voter1, proposalId: new BN(0) });
         });
 
-        it("shouldn't set a vote if it is not a voter", async function() {
-            await VotingInstance.startVotingSession({ from: owner });
-            await expectRevert(VotingInstance.setVote(0, { from: notVoter }),"You're not a voter");
-        });
-
         it("shouldn't set a vote if the voter has already voted", async function() {
-            await VotingInstance.startVotingSession({ from: owner });
-            await VotingInstance.setVote(0, { from: voter1 });
             await expectRevert(VotingInstance.setVote(0, { from: voter1 }),"You have already voted");
         });
 
-        it("shouldn't set a vote if the workflow status is not set at VotingSessionStarted", async function() {
-            await VotingInstance.startVotingSession({ from: owner });
-            await VotingInstance.endVotingSession({ from: owner });
-            await expectRevert(VotingInstance.setVote(0, { from: voter1 }),"Voting session havent started yet");
-        });
-
-        it("shouldn't start the voting session if it is not the owner", async function() {
-            await expectRevert(VotingInstance.startVotingSession({ from: voter1 }),"Ownable: caller is not the owner");
-        });
-
-        it("shouldn't start the voting session if the workflow status is not set at ProposalsRegistrationEnded", async function() {
-            await VotingInstance.startVotingSession({ from: owner });
-            await expectRevert(VotingInstance.startVotingSession(), "Registering proposals phase is not finished");
+        it("shouldn't set a vote if it is not a voter", async function() {
+            await expectRevert(VotingInstance.setVote(0, { from: notVoter }),"You're not a voter");
         });
 
     });
@@ -177,27 +164,26 @@ contract("Voting", function (accounts) {
      // Step 5: Ending Voting Session.
      
      describe("5 --- Ending Voting Session ---", function () {
-        beforeEach(async function () {
+        before(async function () {
             VotingInstance = await Voting.new({ from: owner });
             await VotingInstance.addVoter(voter1, { from: owner });
             await VotingInstance.startProposalsRegistering({ from: owner });
             await VotingInstance.endProposalsRegistering({ from: owner });
         });
 
-        it('should end voting voting session', async () => {
-            await VotingInstance.startVotingSession({ from: owner });
-            await VotingInstance.endVotingSession({ from : owner});
-            currentState = await VotingInstance.workflowStatus();
-            expect(currentState).to.be.bignumber.equal(new BN(4));
+        it("shouldn't end the voting session if the workflow status is not set at VotingSessionStarted", async function() {
+            await expectRevert(VotingInstance.endVotingSession(),"Voting session havent started yet");
         });
 
         it("shouldn't end the voting session if it is not the owner", async function() {
             await VotingInstance.startVotingSession({ from: owner });
             await expectRevert(VotingInstance.endVotingSession({ from: voter1 }),"Ownable: caller is not the owner");
         });
-
-        it("shouldn't end the voting session if the workflow status is not set at VotingSessionStarted", async function() {
-            await expectRevert(VotingInstance.endVotingSession(),"Voting session havent started yet");
+        
+        it('should end voting voting session', async () => {
+            await VotingInstance.endVotingSession({ from : owner});
+            currentState = await VotingInstance.workflowStatus();
+            expect(currentState).to.be.bignumber.equal(new BN(4));
         });
     });
      //Step 6: Votes Tallied.
--- test/readme.md
+++ test/readme.md
@@ -10,43 +10,46 @@ Toutes les fonctions du contrat sont testées
 ### 1) ---Registering Voters---
 
 - should make sure status is at registering voters
+- shouldn't add a voter if it is not the owner
 - should add a voter and emit event
 - shouldn't add a voter if the voter is already registered
-- shouldn't add a voter if it is not the owner
 - shouldn't add a voter if the workflow status is not set at RegisteringVoters
 
 
 ### 2) ---Proposals Registration Started---
 
-- should start proposal registration
 - shouldn't start the proposals registering if it is not the owner
-- shouldn't start the proposals registering if the workflow status is not set at RegisteringVoters
-- should add a proposal and emit event
-- shouldn't add a proposal if it is not a voter
 - shouldn't add a proposal if the workflow status is not set to RegisteringVoters
+- should start proposal registration
+- shouldn't add a proposal if it is not a voter
+- should add a proposal and emit event
+- shouldn't start the proposals registering if the workflow status is not set at RegisteringVoters
 
 
 ### 3) ---Ending Proposal Registration---
 
-- shouldn't end the proposals registering if it is not the owner
 - shouldn't end the proposals registering if the workflow status is not set at ProposalsRegistrationStarted
+- shouldn't end the proposals registering if it is not the owner
 -should end proposal registration
+
 
 ### 4) ---Voting Session Started---
 
-- should start voting session
-- should set a vote and emit event
-- shouldn't set a vote if it is not a voter
-- shouldn't set a vote if the voter has already voted
 - shouldn't set a vote if the workflow status is not set at VotingSessionStarted
 - shouldn't start the voting session if it is not the owner
+- should start voting session
 - shouldn't start the voting session if the workflow status is not set at ProposalsRegistrationEnded
+- should set a vote and emit event
+- shouldn't set a vote if the voter has already voted
+- shouldn't set a vote if it is not a voter
+
 
 ### 5) ---Ending Voting Session---
 
-- should end voting voting session
-- shouldn't end the voting session if it is not the owner
 - shouldn't end the voting session if the workflow status is not set at VotingSessionStarted
+- shouldn't end the voting session if it is not the owner
+- should end voting voting session
+
 
 ### 6) ---Votes Tallied---
 
